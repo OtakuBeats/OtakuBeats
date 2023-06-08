c022717ac8274b837218058df0b601dcedfc4056
Derrick Garcon <dgarconpro@gmail.com>
2023-06-08 19:24:37 -0400

    Early work with sheet filter

--- OtakuBeats/ContentView.swift
+++ OtakuBeats/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
     @State private var searchText = ""
     @State var genres = ["Rock", "Jazz", "Pop"]
     @State var filterCategories = ["Length", "Type", "Date Uploaded", "Clear"]
-    @State private var selectedFilter = " "
+    @State private var selectedFilter = false
+    @State var sheetShowing = false
     
     let anime: [Anime] = Anime.mockAnime
     let songs: [Song] = Song.mockSong
@@ -26,50 +27,79 @@ struct ContentView: View {
         songs.filter { $0.title.localizedCaseInsensitiveContains(searchText) && $0.title.lowercased().hasPrefix(searchText.lowercased()) }
     }
     
+
     var body: some View {
         NavigationView {
             VStack {
                 SearchBar(searchText: $searchText)
-                if selectedFilter == " " {
+                if selectedFilter == false {
                     defaultSearch
                 } else {
                     //filtered search
-                    genreSearch }
+                    Text("hello ") }
             }
             .navigationTitle("Otaku Beats")
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    Menu {
-                        Menu {
-                            Picker("Filter", selection: $selectedFilter)
-                                {
-                                    ForEach(genres, id: \.self) {
-                                        Text($0)
-                                    }
-                                }
-                                .pickerStyle(.automatic)
-                             } label: {
-                                 Text("Genre")
-                             }
-                        
-                        
-                        
-                    Picker("Filter", selection: $selectedFilter)
-                        {
-                            ForEach(filterCategories, id: \.self) {
-                                Text($0)
+                    Button(action: {
+                        sheetShowing.toggle()
+                    }, label: {
+                        Text("Filter")
+                    })
+                    .sheet(isPresented: $sheetShowing) {
+                        VStack {
+                            ForEach(genres, id: \.self) {genre in
+                                Toggle (genre, isOn: $selectedFilter)
                             }
+                            .padding(.horizontal)
                         }
-                        .pickerStyle(.automatic)
-                        .onChange(of: selectedFilter) { value in
-                            if value == "Clear" {
-                                selectedFilter = " "
-                            }
-                            
-                        }
-                } label: {
-                    Text("Filter")
-                }
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+//                    Menu {
+//                        Menu {
+//                            Picker("Filter", selection: $selectedFilter)
+//                                {
+//                                    ForEach(genres, id: \.self) {
+//                                        Text($0)
+//                                    }
+//                                }
+//                                .pickerStyle(.automatic)
+//                             } label: {
+//                                 Text("Genre")
+//                             }
+//
+//
+//
+//                    Picker("Filter", selection: $selectedFilter)
+//                        {
+//                            ForEach(filterCategories, id: \.self) {
+//                                Text($0)
+//                            }
+//                        }
+//                        .pickerStyle(.automatic)
+//                        .onChange(of: selectedFilter) { value in
+//                            if value == "Clear" {
+//                                selectedFilter = " "
+//                            }
+//
+//                        }
+//                } label: {
+//                    Text("Filter")
+//                }
             }
         }
     }
@@ -99,32 +129,32 @@ struct ContentView: View {
     }
     
     
-    var genreSearch: some View {
-        return AnyView (
-            List {
-                switch selectedFilter {
-                case "Rock": ForEach(filteredSongs) { song in
-                    if selectedFilter == "Rock" && song.genre == "Rock" {
-                        Text(song.title)
-                    }
-                }
-                    
-                case "Jazz": ForEach(filteredSongs) { song in
-                    if selectedFilter == "Jazz" && song.genre == "Jazz" {
-                        Text(song.title)
-                    }
-                }
-                
-                case "Pop": ForEach(filteredSongs) { song in
-                    if selectedFilter == "Pop" && song.genre == "Pop" {
-                        Text(song.title)
-                    }
-                }
-                    
-                default: Text("error")
-                    }
-            })
-    }
+//    var genreSearch: some View {
+//        return AnyView (
+//            List {
+//                switch selectedFilter {
+//                case "Rock": ForEach(filteredSongs) { song in
+//                    if selectedFilter == "Rock" && song.genre == "Rock" {
+//                        Text(song.title)
+//                    }
+//                }
+//
+//                case "Jazz": ForEach(filteredSongs) { song in
+//                    if selectedFilter == "Jazz" && song.genre == "Jazz" {
+//                        Text(song.title)
+//                    }
+//                }
+//
+//                case "Pop": ForEach(filteredSongs) { song in
+//                    if selectedFilter == "Pop" && song.genre == "Pop" {
+//                        Text(song.title)
+//                    }
+//                }
+//
+//                default: Text("error")
+//                    }
+//            })
+//    }
     
     
     
