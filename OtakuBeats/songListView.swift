11be5ce90b4f96c2f473d093859b6ecd379e4919
Derrick Garcon <dgarconpro@gmail.com>
2023-06-29 16:48:59 -0400

    moved filter button and functionality to songListView

--- OtakuBeats/songListView.swift
+++ OtakuBeats/songListView.swift
@@ -8,15 +8,52 @@
 import SwiftUI
 
 struct songListView: View {
+    @State var sheetShowing = false
+    @State var selectedGenres = Set<Genres>()
     let anime: Anime
     let songs: [Song]
+    enum Genres: String, CaseIterable {
+        case Rock
+        case Jazz
+        case Pop
+    }
+    
     var body: some View {
-        NavigationView {
+        VStack {
             List {
                 ForEach(songs, id: \.id) { song in
                     NavigationLink(destination: songView(song: song)) {
                         Text(song.title)
                     }
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    sheetShowing.toggle()
+                }, label: {
+                    Text("Filter")
+                })
+            }
+        }
+        .sheet(isPresented: $sheetShowing) {
+            VStack {
+                ForEach(Genres.allCases, id: \.self) {genre in
+                    Toggle (isOn: Binding(
+                        get: {self.selectedGenres.contains(genre)},
+                        set: { newToggleState in
+                            if newToggleState {
+                                selectedGenres.insert(genre)
+                            } else {
+                                selectedGenres.remove(genre)
+                            }}
+                    )) {Text(genre.rawValue)}
+                }
+                .padding(.horizontal)
+                
+                Button("Apply") {
+                    sheetShowing.toggle()
                 }
             }
         }
